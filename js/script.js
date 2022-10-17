d8988fb316edf799af2d60f63de7ed0f751b740d
Eduardo Chaves <henriqueduardo2002@gmail.com>
2022-10-17 05:58:10 -0300

    accomplished sidebar animation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-// a
+const sidebar = document.querySelector('#sidebar');
+const btnHamburgerMenu = document.querySelector('#btn-hamburger');
+
+btnHamburgerMenu.addEventListener('click', () => {
+  sidebar.classList.toggle('sidebar-closed');
+})
